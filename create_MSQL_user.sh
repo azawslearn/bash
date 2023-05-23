e4a37a949dc57a67bdc478c3433fb595958445af
azawslearn <134204024+azawslearn@users.noreply.github.com>
2023-05-23 16:05:08 +0300

    Update create_MSQL_user.sh

--- create_MSQL_user.sh
+++ create_MSQL_user.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-MYSQL_ROOT_PASSWORD='YourRootPassword'
-MYSQL_USER='root'
-MYSQL_USER_PASSWORD='UserPassword'
+MYSQL_ROOT_PASSWORD='EmersonFitipaldi'
+MYSQL_USER='my_user'
+MYSQL_USER_PASSWORD='EmersonFitipaldi'
 
 # Create the MySQL user
-mysql -u root -p$MYSQL_ROOT_PASSWORD -e "CREATE USER '$MYSQL_USER'@'localhost' IDENTIFIED BY '$MYSQL_USER_PASSWORD';"
+mysql -u root -p$MYSQL_ROOT_PASSWORD -e "CREATE USER '$MYSQL_USER' IDENTIFIED BY '$MYSQL_USER_PASSWORD';"
 
 # Grant all privileges to the user
-mysql -u root -p$MYSQL_ROOT_PASSWORD -e "GRANT ALL PRIVILEGES ON *.* TO '$MYSQL_USER'@'localhost' WITH GRANT OPTION;"
+mysql -u root -p$MYSQL_ROOT_PASSWORD -e "GRANT ALL PRIVILEGES ON *.* TO '$MYSQL_USER'@' WITH GRANT OPTION;"
 
 # Flush privileges
 mysql -u root -p$MYSQL_ROOT_PASSWORD -e "FLUSH PRIVILEGES;"
